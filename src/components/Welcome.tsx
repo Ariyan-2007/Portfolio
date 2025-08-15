fa7d3e6f2f44498b16ea65c76d39a856f6ab4e1d
Ariyan-2007 <ariyanjahangireng@gmail.com>
2025-08-16 04:56:56 +0600

    Astro Init

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import '../styles/global.css';
+import { Button } from "./ui/button";
+
+export default function Welcome() {
+
+ const [count, setCount] = useState(0);
+ 
+
+
+
+  return (
+    <div className="justify-center align-middle items-center flex">
+      <div>
+        <Button className="hover:cursor-pointer" onClick={() =>setCount(count+1)}>Press Here:</Button>
+      </div>
+      <div>{count}</div>
+    </div>
+  )
+}
